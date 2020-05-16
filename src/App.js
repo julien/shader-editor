0c470822063dc88155d9ba773610acad807786fe
Julien Castelain <julien.castelain@liferay.com>
2020-05-16 16:50:07 +0200

    Rename a few variables and monitor test changes

--- src/App.js
+++ src/App.js
@@ -38,16 +38,17 @@ function App() {
 	const W = useRef(0);
 	const H = useRef(0);
 
-	let gl,
-		lastTime = 0,
-		u_mouse = [0.5, 0.5],
-		u_time = 0;
+	let gl;
+	let lastTime = 0;
+	let mouse = [0.5, 0.5];
+	let time = 0;
+	let lastText = DEFAULT_FRAGMENT_SHADER;
 
 	const info = {
-		a_position: null,
-		u_mouse: null,
-		u_resolution: null,
-		u_time: null
+		position: null,
+		mouse: null,
+		resolution: null,
+		time: null,
 	};
 
 	function initGL() {
@@ -74,34 +75,34 @@ function App() {
 		gl.linkProgram(prog);
 		gl.useProgram(prog);
 
-		info.a_position = gl.getAttribLocation(prog, "a_position");
-		info.u_mouse = gl.getUniformLocation(prog, "u_mouse");
-		info.u_resolution = gl.getUniformLocation(prog, "u_resolution");
-		info.u_time = gl.getUniformLocation(prog, "u_time");
+		info.position = gl.getAttribLocation(prog, "a_position");
+		info.mouse = gl.getUniformLocation(prog, "u_mouse");
+		info.resolution = gl.getUniformLocation(prog, "u_resolution");
+		info.time = gl.getUniformLocation(prog, "u_time");
 	}
 
 	function initBuffers() {
-		gl.enableVertexAttribArray(info.a_position);
+		gl.enableVertexAttribArray(info.position);
 		gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
 		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-		gl.vertexAttribPointer(info.a_position, 2, gl.FLOAT, gl.FALSE, 0, 0);
+		gl.vertexAttribPointer(info.position, 2, gl.FLOAT, gl.FALSE, 0, 0);
 		gl.drawArrays(gl.TRIANGLE_STRIP, 0, positions.length / 2);
 	}
 
 	function setUniforms() {
-		gl.uniform1f(info.u_time, u_time);
-		gl.uniform2f(info.u_mouse, u_mouse[0], u_mouse[1]);
-		gl.uniform2f(info.u_resolution, W.current, H.current);
+		gl.uniform1f(info.time, time);
+		gl.uniform2f(info.mouse, mouse[0], mouse[1]);
+		gl.uniform2f(info.resolution, W.current, H.current);
 	}
 
 	function loop() {
 		rafRef.current = requestAnimationFrame(loop);
 
-		const now = new Date().getTime();
+		const now = Date.now();
 		if (lastTime) {
 			const elapsed = now - lastTime;
 
-			u_time += (2.0 * elapsed) / 1000.0;
+			time += (2.0 * elapsed) / 1000.0;
 		}
 		lastTime = now;
 		draw();
@@ -109,16 +110,16 @@ function App() {
 
 	function draw() {
 		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-		u_time += 0.01;
-		gl.uniform1f(info.u_time, u_time);
+		time += 0.01;
+		gl.uniform1f(info.time, time);
 		gl.drawArrays(gl.TRIANGLE_STRIP, 0, positions.length / 2);
 	}
 
-	const handleMouseMove = useCallback(e => {
-		u_mouse[0] = e.pageX;
-		u_mouse[1] = e.pageY * -1;
+	const handleMouseMove = useCallback((e) => {
+		mouse[0] = e.pageX;
+		mouse[1] = e.pageY * -1;
 
-		gl.uniform2f(info.u_mouse, u_mouse[0], u_mouse[1]);
+		gl.uniform2f(info.mouse, mouse[0], mouse[1]);
 		/* eslint-disable react-hooks/exhaustive-deps */
 	}, []);
 
@@ -153,19 +154,29 @@ function App() {
 		/* eslint-disable react-hooks/exhaustive-deps */
 	}, []);
 
+	function handleBlur(e) {
+		const currentText = e.target.innerHTML;
+		if (currentText !== lastText) {
+		}
+	}
+
+	function handleFocus(e) {
+		lastText = e.target.innerHTML;
+	}
+
 	return (
-		<div>
-			<div className="main">
-				<div className="editor">
-					<pre>
-						<code
-							contentEditable={true}
-							dangerouslySetInnerHTML={{ __html: DEFAULT_FRAGMENT_SHADER }}
-							ref={editorRef}
-						></code>
-					</pre>
-				</div>
-				<canvas ref={canvasRef}></canvas>
+		<div className="main">
+			<canvas ref={canvasRef}></canvas>
+			<div className="editor">
+				<pre>
+					<code
+						contentEditable={true}
+						dangerouslySetInnerHTML={{ __html: DEFAULT_FRAGMENT_SHADER }}
+						ref={editorRef}
+						onBlur={handleBlur}
+						onFocus={handleFocus}
+					></code>
+				</pre>
 			</div>
 		</div>
 	);
